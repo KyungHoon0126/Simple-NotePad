0ec7aa7ff1ff857b87649820b8b1c2f3b82d3a36
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-10-19 11:54:10 +0900

    Add files via upload

---
+++ A Simple NotePad/A Simple NotePad/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace A_Simple_NotePad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string file = ""; // File Path
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "WPF Simple NotePad";
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofdlg = new OpenFileDialog(); // File Open
+            {
+                ofdlg.Filter = "txt File | *.txt"; // File Filter Set
+
+                ofdlg.CheckFileExists = true;
+                ofdlg.CheckPathExists = true;
+
+                if (ofdlg.ShowDialog().GetValueOrDefault()) // File Open, CheckValueExists
+                {
+                    file = ofdlg.FileName;
+                    this.Title = file;
+
+                    using (StreamReader sw = new StreamReader(file))
+                    {
+                        tbWrite.Text = sw.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        private void Remove(object sender, RoutedEventArgs e)
+        {
+            tbWrite.Text = string.Empty;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            if (file == "")
+            {
+                SaveFileDialog sfdlg = new SaveFileDialog();
+                sfdlg.Filter = "Text File | *.txt";  // File Filter Set
+
+                if (sfdlg.ShowDialog().GetValueOrDefault())
+                {
+                    file = sfdlg.FileName;
+                    this.Title = file;
+                }
+                else return;
+            }
+
+            // StreamWriter(FileName, CheckContentAdd(false : All Clear, Encoding))
+            using (StreamWriter sw = new StreamWriter(file, false, Encoding.UTF8))
+            {
+                sw.Write(tbWrite.Text);
+            }
+        }
+    }
+}
